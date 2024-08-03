e86277ad9c3021e2af22fb2a6d8f8220f6c89ce8
MOHIT <mohitjyouti2003@gmail.com>
2024-08-03 12:55:58 +0530

    add files

--- cloudinaryConfig.js
+++ cloudinaryConfig.js
@@ -1,22 +1,21 @@
-const cloudinary = require('cloudinary').v2;
-const { CloudinaryStorage } = require('multer-storage-cloudinary');
-
+const cloudinary = require("cloudinary").v2;
+const { CloudinaryStorage } = require("multer-storage-cloudinary");
 
 cloudinary.config({
-    cloud_name: "dgepdwzt0" ,
-    api_key: 711379229351361,
-    api_secret:"NUldIIyu9u18pRidHI1oPTuTPp8" ,
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUD_API_KEY,
+  api_secret: process.env.CLOUD_API_SECRET,
 });
 
 const storage = new CloudinaryStorage({
-    cloudinary: cloudinary,
-    params: {
-      folder: 'wanderlust_DEV',
-      allowerdformats: ["png","jpg","jpeg"],
-    },
-  });
+  cloudinary: cloudinary,
+  params: {
+    folder: "wanderlust_DEV",
+    allowerdformats: ["png", "jpg", "jpeg"],
+  },
+});
 
-module.exports ={
-    cloudinary,
-    storage,
-}
+module.exports = {
+  cloudinary,
+  storage,
+};
